2700522a7aab8b676c48c959021bb7bda47b1217
Salvatore949 <salvo.mess49@gmail.com>
2021-12-17 16:32:03 +0100

    value printed

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <?php
+      $x = 10;
+      $y = 20;
+      $z = $x + $y;
+      ?>
+    
+    <h1>Hello World <?php echo $z; ?></h1>
+    
+</body>
+</html>
